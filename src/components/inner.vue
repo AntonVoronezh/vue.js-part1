e02627d88aadf2a14d33bb392a4a42fea4db1d12
AntonVoronezh <pstut@mail.ru>
2019-01-18 13:14:42 +0300

    v-model

--- src/components/inner.vue
+++ src/components/inner.vue
@@ -7,6 +7,26 @@ div
     button(
       v-on="$listeners"
     ) click
+    br
+    br
+    br
+    div {{formData}}
+      br
+      input(type="text" v-model="formData.userName")
+      span hello
+      br
+      input(type="checkbox" value="music" v-model="formData.hobby") 
+      span music
+      input(type="checkbox" value="kino" v-model="formData.hobby") 
+      span kino
+      br
+      input(type="radio" name="payment" value="mastercard"  v-model="formData.payment") 
+      span mastercard
+      input(type="radio" name="payment" value="visa"  v-model="formData.payment") 
+      span visa
+      br
+      select(v-model="formData.number")
+        option(v-for="n in 10" :value="n" ) {{n}}
 </template>
 
 <script>
@@ -14,7 +34,14 @@ export default {
   inheritAttrs: false,
   data() {
     return {
-      msg: "-------------p"
+      msg: "-------------p",
+      title: "ss",
+      formData: {
+        userName: "",
+        hobby:[],
+        payment: "",
+        number: ""
+      }
     };
   },
   mounted() {
